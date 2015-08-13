a73a6d3eaef402e819683f6a8be4c520125d222e
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-08-13 23:35:15 +0900

    Problem5-5 : Make data structuer

---
+++ 5_DynamicProgramming/5_Tree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define STR 5
+#define DEG 5
+#define LEN 100
+
+typedef struct {
+  char label[STR];
+  int upp;
+  int deg;
+  int child[DEG];
+} Node;
+
+int main(void) {
+
+  return 0;
+}
